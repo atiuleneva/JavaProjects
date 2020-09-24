4f77a208dc5a2d9028d73fc671f86217537b1707
atiuleneva <anna.tiuleneva@outlook.com>
2020-09-24 14:29:24 +0300

    dz08

--- dz08/src/main/java/org/atiuleneva/dz08/Main.java
+++ dz08/src/main/java/org/atiuleneva/dz08/Main.java
@@ -1,7 +1,12 @@
 package org.atiuleneva.dz08;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args){
-
+        UIManager.put("Button.disabledText", Color.blue);
+        MyGame myGame = new MyGame();
+        myGame.setVisible(true);
     }
 }
